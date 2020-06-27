0df3594638b432c113a6067f4faf2428db7a2683
Sebastián Jaimovich <sebaj80@gmail.com>
2020-06-27 20:48:08 -0300

    fix de api get pos

--- api/pos/get/index.php
+++ api/pos/get/index.php
@@ -18,6 +18,6 @@ $url="https://api.mercadopago.com/pos";
 
 
 //curl_call("get","$url/$external_id?access_token=$access_token","");
-curl_call("get","$url?access_token=$access_token","");
+curl_call("get","$url?external_id=$external_id&access_token=$access_token","");
 
 ?>
