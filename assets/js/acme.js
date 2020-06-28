5772531b5552bd1fe446ed84afd2d895377ca158
Sebastián Jaimovich <sebaj80@gmail.com>
2020-06-28 00:56:47 -0300

    fix js

--- assets/js/acme.js
+++ assets/js/acme.js
@@ -130,7 +130,7 @@ $(document).ready(function() {
 									if(orderStatus=="closed"){
 										if(cashSound){playSound("cash")};
 										cashSound=false;
-										setTimeout(clearInterval(checkStatusNotif),3000);
+										setTimeout(clearInterval(checkStatus),3000);
 
 										$('#orderFinalStatus').text(elements[totalElements-1]);
 										$('#exampleModal').modal("hide");
